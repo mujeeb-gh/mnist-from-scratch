6350aca8be85fe242f8bde599987fbdbf7963be1
mujeeb-gh <olamidebalogun174@gmail.com>
2026-01-29 23:55:24 +0100

    Add SGD

--- src/network.py
+++ src/network.py
@@ -17,6 +17,23 @@ class Network(object):
             a = sigmoid(np.dot(w, a) + b)
         return a
     
+    def SGD(self, training_data, epochs, mini_batch_size, eta, test_data=None):
+        if test_data:
+            n_test = len(test_data)
+        n = len(training_data)
+        for j in range(epochs):
+            random.shuffle(training_data)
+            mini_batches = [
+                training_data[k: k + mini_batch_size]
+                for k in range(0, n, mini_batch_size)
+            ]
+            for mini_batch in mini_batches:
+                self.update_mini_batch(mini_batch, eta)
+            if test_data:
+                print(f"Epoch {j}: {self.evaluate(test_data)} / {n_test}")
+            else:
+                print(f"Epoch {j} complete")
+    
      
 def sigmoid(z):
     return 1.0 / (1.0 + np.exp(-z))
