0b635cf10e86f59cbe1a8c74f1b770b019c123cd
mujeeb-gh <olamidebalogun174@gmail.com>
2026-02-02 23:58:05 +0100

    Update backprop

--- src/network.py
+++ src/network.py
@@ -47,8 +47,31 @@ class Network(object):
         self.biases = [b - (eta / len(mini_batch) * nb) for b, nb in zip(self.biases, nabla_b)]
         
     def backprop(self, x, y):
-        pass
-    
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+        
+        # feedforward
+        activation = x
+        activations = [x]
+        zs = []
+        for b, w in zip(self.biases, self.weights):
+            z = np.dot(w, activation) + b
+            zs.append(z)
+            activation = sigmoid(z)
+            activations.append(activation)
+            
+        # backward pass
+        delta = self.cost_derivative(activations[-1], y) * sigmoid_prime(zs[-1])
+        nabla_b[-1] = delta
+        nabla_w = np.dot(delta, activations[-2].transpose())
+        
+        for l in range(2, self.num_layers):
+            z = zs[-1]
+            sp = sigmoid_prime(z)
+            delta = np.dot(self.weights[-l + 1].transpose(), delta) * sp
+            nabla_b[-1] = delta
+            nabla_w[-1] = np.dot(delta, activations[-l-1].transpose())
+        return(nabla_b, nabla_w)      
      
 def sigmoid(z):
     return 1.0 / (1.0 + np.exp(-z))
